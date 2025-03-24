21c23f96732a9a1f997ba91503e66747a5f168c8
MAX <1992mmaximm1992@gmail.com>
2025-03-24 08:07:11 +0300

    first commit

---
+++ classWork/classWork.js
@@ -0,0 +1,32 @@
+//Вывести в консоль заданную строку N раз
+
+// const text= "заданную строку";
+// const N=12;
+// for (i = 0; i < N; i++) {
+//     console.log(text)
+// }
+
+// let number = 20;
+// for (let i = 1; i <= number; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i**2,'чётное');
+//     } else { console.log(i**2,'нечётное')}
+// }
+
+// let N = 100
+// let M = 10
+//  for (let i = 1; N>M; i++) {
+//        N=N / 2;
+//      if (N>M) {console.log(i,N);}
+//  }
+
+// let start = 1800;
+// let end = 2020;
+// let iteration = 0;
+//
+// for (let i=start; i<=end; i++){
+//     iteration++
+//     if (i==1961) {
+//         console.log('год первого полета в космос - 1961, кол-во итераций=', iteration);
+//     }
+// }
