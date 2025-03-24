862047d38d925b04341b7e5a3adce7b37bb7f417
PetrovMaxim1992 <1992mamximm1992@gmail.com>
2025-03-24 21:23:23 +0300

    test

--- euler.jakumo.org/2#.js
+++ euler.jakumo.org/2#.js
@@ -5,53 +5,53 @@
 //*********************************************************************************************************
 
 //--------------------------С промежуточной суммой по циклам
-let first=0;
-let second=1;
-let sum=0;
-let sumCheck
-console.log(first);
-console.log(second);
-console.log('sum=',sum);
-for (let i = 1; sum <= 4*10**6; i++) {
-    first = first + second;
-    second = second + first;
-    sumCheck=sum;
-    if (first % 2 === 0 && sum <= 4*10**6) {
-        sum = sum + first;
-    }
-    if (second % 2 === 0 && sum <= 4*10**6) {
-        sum = sum + second;
-    }
-    if (sum <= 4*10**6) {
-        console.log(i,'цикл');
-        console.log('first=',first);
-        console.log('second=',second);
-        console.log('sum=',sum);
-    }
-    if (sum >= 4*10**6) {
-        console.log(sumCheck)
-        break
-    }
-}
-
-//--------------------------Решение по ТЗ задачи
 // let first=0;
 // let second=1;
 // let sum=0;
 // let sumCheck
-// for (let i = 0; sum <= 4*10**6; i++) {
+// console.log(first);
+// console.log(second);
+// console.log('sum=',sum);
+// for (let i = 1; sum <= 4*10**6; i++) {
 //     first = first + second;
 //     second = second + first;
 //     sumCheck=sum;
-//     if (first % 2 === 0) {
+//     if (first % 2 === 0 && sum <= 4*10**6) {
 //         sum = sum + first;
 //     }
-//     if (second % 2 === 0) {
+//     if (second % 2 === 0 && sum <= 4*10**6) {
 //         sum = sum + second;
+//     }
+//     if (sum <= 4*10**6) {
+//         console.log(i,'цикл');
+//         console.log('first=',first);
+//         console.log('second=',second);
+//         console.log('sum=',sum);
 //     }
 //     if (sum >= 4*10**6) {
 //         console.log(sumCheck)
 //         break
 //     }
 // }
+
+//--------------------------Решение по ТЗ задачи
+let first=0;
+let second=1;
+let sum=0;
+let sumCheck
+for (let i = 0; sum <= 4*10**6; i++) {
+    first = first + second;
+    second = second + first;
+    sumCheck=sum;
+    if (first % 2 === 0) {
+        sum = sum + first;
+    }
+    if (second % 2 === 0) {
+        sum = sum + second;
+    }
+    if (sum >= 4*10**6) {
+        console.log(sumCheck)
+        break
+    }
+}
 //********************************************Конец задачи №2***************************************
--- euler.jakumo.org/3#.js
+++ euler.jakumo.org/3#.js
@@ -3,16 +3,34 @@
 //Каков самый большой делитель числа 600851475143, являющийся простым числом?
 //*********************************************************************************************************
 
-let simpleDel = 17;
-for (let simpleNum = 1; simpleNum <= 17; simpleNum++) {
-    for (let i = 1; i <= simpleNum; i++) {
-        if (simpleNum % i === 0) {
-            if (i !== 1 && i !== simpleNum || simpleNum === 1) {
-                console.log(simpleNum, '-не простое число')
+// let necNum = 60;
+// for (let simpleNum = 1; simpleNum <= necNum; simpleNum++) { //цикл прогоняет все числа от 1 до necNum
+//     for (let i = 1; i <= simpleNum; i++) { //цикл делит каждый simpleNum на число от 1 до simpleNum
+//         if (simpleNum % i === 0) { //проверка на деление без остатка
+//             if (i !== 1 && i !== simpleNum || simpleNum === 1) { //проверка на непростое число. 1 это
+//                 // непростое число. число не простое, если делится на что-то кроме себя и единицы.
+                // console.log(simpleNum, '-непростое число')
+//                 break
+//             }
+//             if (i === simpleNum) {
+//                 console.log(simpleNum, '-простое число')
+//             }
+//         }
+//     }
+// }
+
+let necNum = 2;
+for (let simpleNum = 2; simpleNum <= necNum; simpleNum++) { //цикл прогоняет все числа от 2 до necNum
+    for (let i = 2; i <= simpleNum; i++) { //цикл делит каждый simpleNum на число от 2 до simpleNum
+        if (simpleNum % i === 0) { //проверка на деление без остатка
+            if (i !== simpleNum) { //проверка на непростое число. 1 это
+                // непростое число. число не простое, если делится на что-то кроме себя и единицы.
+                // console.log(simpleNum, '-непростое число')
                 break
             }
-            if (i === simpleNum) {
-                console.log(simpleNum, '-простое число')
+            if (i === simpleNum && necNum % simpleNum === 0) {
+                console.log(simpleNum);
+                // console.log(simpleNum, '-простое число')
             }
         }
     }
