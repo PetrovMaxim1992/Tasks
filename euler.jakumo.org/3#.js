2cac06d5678a007006089e748c74789ca6b4c25b
MAX <1992mmaximm1992@gmail.com>
2025-03-25 14:17:48 +0300

    test

--- euler.jakumo.org/3#.js
+++ euler.jakumo.org/3#.js
@@ -23,22 +23,20 @@
 //     }
 // }
 
-let necNum = 2;
+console.time()
+let verNum;
+let simpleNum;
+let necNum = 255;
 for (let simpleNum = 2; simpleNum <= necNum; simpleNum++) { //цикл прогоняет все числа от 2 до necNum
-    for (let i = 2; i <= simpleNum; i++) { //цикл делит каждый simpleNum на число от 2 до simpleNum
-        if (simpleNum % i === 0) { //проверка на деление без остатка
-            if (i !== simpleNum) { //проверка на непростое число. 1 это
-                // непростое число. число не простое, если делится на что-то кроме себя и единицы.
-                // console.log(simpleNum, '-непростое число')
-                break
-            }
-            if (i === simpleNum) {
-                console.log(simpleNum, '-простое число')
-                if (i === simpleNum && necNum % simpleNum === 0) {
+    if (simpleNum % 2 !==0 && necNum % simpleNum === 0) {
+        for (let verNum = 2; verNum <= simpleNum; verNum++) {
+            if (simpleNum % verNum === 0) {
+                if (simpleNum === verNum) {
                     console.log(simpleNum);
-                    // console.log(simpleNum, '-простое число')
                 }
+                break;
             }
         }
     }
 }
+console.timeEnd()
